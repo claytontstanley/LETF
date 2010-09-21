b977933ffb803d0708f7399f1691496f87ecf052
clayton stanley <claytonstanley@mobile-166-217-074-045.mycingular.net>
2010-09-21 18:33:04 -0400

    ensured backward compatibility between old and new compilers

--- docs/htmlConfig.lisp
+++ docs/htmlConfig.lisp
@@ -1,8 +1,17 @@
 (require 'asdf)
-(asdf:disable-output-translations)
+
+;if we're using the new asdf compiler, disable-output-translations so that compiled fasl files
+;reside next to the source files (like the older asdf compiler did)
+(in-package :asdf)
+(if (fboundp  'disable-output-translations)
+    (disable-output-translations))
+
+;jump back in to the default common-lisp-user package
+(in-package :common-lisp-user)
 
 (load (format nil "~a/cldoc/cldoc.asd"
 	(get-pandoric #'args 'platform)))
+
 (require 'cldoc)
 
 (defun generate-docs ()
